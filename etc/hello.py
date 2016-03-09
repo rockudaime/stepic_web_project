cf957ff8e376d28bdeb70cfdc3034398748f6f35
Sergey Bazarov <rockudaime@users.noreply.github.com>
2016-03-09 22:24:09 +0200

    Changed mistyping in hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,6 +1,6 @@
 CONFIG = {                                                                      
     'mode': 'wsgi',                                                           
-    'working_dir': '/home/box/mweb/hello.py',                                           
+    'working_dir': '/home/box/web/hello.py',                                           
     'python': '/usr/bin/python',                                              
     'args': (                                                                   
         '--bind=127.0.0.1:8080',                                              
