03f3036913c8d0aeefeefd8cceca6a62df191810
Sergey Bazarov <rockudaime@users.noreply.github.com>
2016-03-10 21:52:56 +0200

    updated qunicorn conf file

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/helo.py
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
