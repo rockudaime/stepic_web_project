35632c73fcf81f9d37425dd99a9a3f5ea5a0e545
rockudaime <rockudaime@users.noreply.github.com>
2016-03-10 22:27:56 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart -b 0.0.0.0:8080 hello:app
+sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 hello:app
